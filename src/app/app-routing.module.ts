41a9301d1b51a6cca1761bea31e920323d567b3c
Matthew Harvell <matthew.harvell@matthewharvell.com>
2018-08-06 17:17:24 -0500

    beginning routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginmainComponent } from './pages/loginmain/loginmain.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ForgotpasswordComponent } from './components/forgotpassword/forgotpassword.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginmainComponent},
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'forgotpassword', component: ForgotpasswordComponent},
+  /*{ path: 'step4', component: Step4Component},
+  { path: 'step5', component: Step5Component},
+  { path: 'step6', component: Step6Component} */
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
